88849530c99d013b12906b0e5425a69990ef99af
avneesh-star <59157024+avneesh-star@users.noreply.github.com>
2020-10-01 17:19:57 +0530

    add list.py

---
+++ list.py
@@ -0,0 +1,10 @@
+thislist = ["Raj", "Akash", "Param"]
+print(thislist)
+thislist[2] = "Rakesh"
+print(thislist)
+thislist.append("Prakash") #add a value in last 
+print(thislist)
+thislist.insert(1, "Raju") #add a value in a specipic position
+print(thislist)
+thislist.remove("Akash") #remove a value from the list
+print(thislist)
